5608b4b06e9b5c32c1f4e4d5a6cfb612ddef77b5
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-06-06 16:34:20 +0200

    array with object

--- js/myscript.js
+++ js/myscript.js
@@ -3,13 +3,33 @@ createApp({
     data() {
         return {
             todoList: [
-                "pasta",
-                "riso",
-                "latte",
-                "uova",
-                "cioccolato",
-                "cereali",
-            ]
+                {
+                    text: "pasta",
+                    done: "true",
+                },
+                {
+                    text: "pollo",
+                    done: "false",
+                },
+                {
+                    text: "riso",
+                    done: "false",
+                },
+                {
+                    text: "latte",
+                    done: "false",
+                },
+                {
+                    text: "cioccolata",
+                    done: "true",
+                },
+                {
+                    text: "uova",
+                    done: "true",
+                }
+                
+            ],
+            newElement: "",
             
         }
     }
